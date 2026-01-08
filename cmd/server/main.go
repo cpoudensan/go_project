ce09d2fd4f3138fc0e664df13a495ce85f56fba7
Loujaine Lechehab <llachheb0@gmail.com>
2026-01-08 11:48:35 +0100

    server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"strings"
 )
 
 func handleClient(conn net.Conn) {
@@ -12,12 +13,43 @@ func handleClient(conn net.Conn) {
 	r := bufio.NewReader(conn)
 
 	for {
-		line, err := r.ReadString('\n') // attend une ligne
+		line, err := r.ReadString('\n')
 		if err != nil {
-			return
+			return // client fermé
 		}
-		fmt.Println("Received:", line)
-		io.WriteString(conn, "OK\n")
+
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+
+		parts := strings.Fields(line)
+		if len(parts) != 3 {
+			io.WriteString(conn, "ERR bad_request\n")
+			continue
+		}
+
+		if strings.ToUpper(parts[0]) != "ROUTE" {
+			io.WriteString(conn, "ERR unknown_cmd\n")
+			continue
+		}
+
+		from := parts[1]
+		to := parts[2]
+
+		path, dist, err := Dijkstra(Graph, from, to)
+		if err != nil {
+			if err == ErrUnknownCity {
+				io.WriteString(conn, "ERR unknown_city\n")
+			} else if err == ErrNoRoute {
+				io.WriteString(conn, "ERR no_route\n")
+			} else {
+				io.WriteString(conn, "ERR internal\n")
+			}
+			continue
+		}
+
+		io.WriteString(conn, fmt.Sprintf("OK dist=%d path=%s\n", dist, strings.Join(path, "->")))
 	}
 }
 
@@ -29,7 +61,7 @@ func main() {
 	fmt.Println("Listening on :8000")
 
 	for {
-		conn, err := ln.Accept() // attend un client
+		conn, err := ln.Accept()
 		if err != nil {
 			fmt.Println("accept error:", err)
 			continue
